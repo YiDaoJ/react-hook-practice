25a0e6a2acb36e52fac3ac1cf22e373e60623021
YiDao <jcxsusan@gmail.com>
2023-04-02 17:11:50 +0200

    commit main

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,12 @@ import { List } from "./List";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [dark, setDark] = useState(false);
-
-  // every time when dark is updated, App will be re-rendered
-  // getItems will be re-created
-  // in order to avoid re-creating by getItems
-  // getItems should only depends on the count state
-  // BEFORE:
-  // const getItems = () => {
-  //   return [count, count + 1];
-  // };
-  // AFTER:
-  const getItems = useCallback(() => {
-    return [count, count + 1];
-  }, [count]);
 
   return (
     <div className="App">
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
-      <List getItems={getItems} />
-      <button onClick={() => setDark(!dark)}>Toggle</button>
     </div>
   );
 }
