85530b1b719e7a4073479258406d47c1920eb626
YiDao <jcxsusan@gmail.com>
2023-04-24 09:34:28 +0200

    test signal hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { List } from "./List";
+import { signal, effect } from "@preact/signals-react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("test");
+  });
+  // const [count, setCount] = useState(0);
+  const count = signal(0);
+  effect(() => {
+    console.log("ConterWithSignals Rerendered!!");
+    console.log(count.value);
+  });
 
   return (
     <div className="App">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <button onClick={() => count.value++}>count is {count}</button>
     </div>
   );
 }
